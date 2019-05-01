42472a7f4cdb9e08061b62ae9107d2240e010647
Jonas <au544262@post.au.dk>
2019-05-01 10:41:20 +0200

    Changed stuff

---
+++ PB.Dto/EventDTOs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB.Dto
+{
+
+    public class EventModel
+    {
+        [Required] public string Location { get; set; }
+        [Required] public string Description { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] public DateTime StartDate { get; set; }
+        [Required] public DateTime EndDate { get; set; }
+        public string Pin { get; set; }
+    }
+
+    public class EventPinModel
+    {
+        public string pin { get; set; }
+    }
+
+    public class EditEventModel
+    {
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ PB.Dto/PictureDTOs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PB.Dto
+{
+    public class InsertPictureModel
+    {
+
+        [Required]
+        public string PictureString { get; set; }
+
+        [Required] public string EventPin { get; set; }
+
+    }
+
+    public class DeletePictureModel
+    {
+        [Required]
+        public string EventPin { get; set; }
+        [Required]
+        public int PictureId { get; set; }
+    }
+
+    public class PicturesAnswerModel
+    {
+        [Required]
+        public List<int> PictureList { get; set; }
+    }
+}
